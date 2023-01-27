3ca36ce4a882781df1c35ad27fc40c810b7d8172
thewildandy <thewildandy@gmail.com>
2023-01-27 12:05:56 +0000

    Connect to firebase and load our questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,56 @@
 import './main.css'
 
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc, collection, query, getDocs } from "firebase/firestore";
+import { useEffect, useState } from 'react';
+
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAju55i18o-IQSelNHFtZMfxhJd70AFcAM",
+    authDomain: "basil-food-quiz-lesson.firebaseapp.com",
+    projectId: "basil-food-quiz-lesson",
+    storageBucket: "basil-food-quiz-lesson.appspot.com",
+    messagingSenderId: "458261520143",
+    appId: "1:458261520143:web:d68ec27b1000d52df2a87a"
+  };
+  const app = initializeApp(firebaseConfig)
+  const db = getFirestore(app)
+
+  const [questions, setQuestions] = useState([])
+
+  /**
+   * Retrieves questions from the firestore.
+   */
+  async function loadQuestions(): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      const snapshot = await getDocs(collection(db, "__dev", "datastore", "questions"))
+      const questions = []
+      snapshot.forEach((doc) => {
+        questions.push(doc.data())
+      })
+      resolve(questions)
+    })
+  }
+
+  /**
+   * On component first render, load our questions.
+   */
+  useEffect(() => {
+    loadQuestions().then((questions) => {
+      setQuestions(questions)
+    })
+  }, [])
+
   return (
     <div className="flex mt-6 justify-center">
       <div className="bg-gray-600 text-white text-xl p-6" style={{width: '400px'}}>
         <h2 className="font-bold">Food Quiz!</h2>
-        {[1,2,3].map(q => 
-          <div className="">
-            <label className="block">Question text</label> 
+        {questions.map((q, i) => 
+          <div className="" key={i}>
+            <label className="block">{q.text}</label> 
             <input type="text" className="block"/>
-          </div>  
+          </div>
         )}
         <button className="bg-indigo-500 rounded-md p-2 mt-4">Submit!</button>
       </div>

f33dbbebdb19167e0ff62ee85b5d0b32d98077d0
thewildandy <thewildandy@gmail.com>
2023-01-27 13:19:55 +0000

    Submit the answers to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './main.css'
 
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc, collection, query, getDocs } from "firebase/firestore";
+import { getFirestore, doc, getDoc, collection, query, getDocs, setDoc, addDoc } from "firebase/firestore";
 import { useEffect, useState } from 'react';
 
 const firebaseConfig = {
@@ -33,6 +33,7 @@ function App() {
 function FormController() {
 
   const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState([])
 
   /**
    * Retrieves questions from the firestore.
@@ -40,9 +41,11 @@ function FormController() {
   async function loadQuestions(): Promise<any> {
     return new Promise(async (resolve, reject) => {
       const snapshot = await getDocs(collection(db, "__dev", "datastore", "questions"))
-      const questions = []
-      snapshot.forEach((doc) => {
-        questions.push(doc.data())
+      const questions: any[] = []
+      snapshot.forEach((doc: any) => {
+        const question = { ...doc.data() }
+        question._id = doc.id
+        questions.push(question)
       })
       resolve(questions)
     })
@@ -56,13 +59,64 @@ function FormController() {
       setQuestions(questions)
     })
   }, [])
+
+  /**
+   * Update our FormController's list of answers, so it 
+   * always reflects the current values for all questions
+   */
+  function onAnswerChange(questionId, answer) {
+    const newAnswer = {
+      questionId: questionId,
+      answerText: answer
+    }
+
+    setAnswers(currentValues => {
+      const updatedAnswers = [ ... currentValues ]
+
+      // Check if we already have an answer for this question
+      const existingAnswer = updatedAnswers.findIndex((a) => a.questionId === newAnswer.questionId)
+      if (existingAnswer >= 0) {
+        // update the answer
+        updatedAnswers[existingAnswer] = newAnswer
+      } else {
+        // create a new answer entry
+        updatedAnswers.push(newAnswer)
+      }
+
+      // updatedAnswers.push(newAnswer)
+      return updatedAnswers
+    })
+  }
+
+  /**
+   * Submit the answers to the database
+   * Maybe do validation first
+   * Display a success or error message
+   */
+  async function processSubmission() {
+    const ref = await addDoc(collection(db, "__dev", "datastore", "answers"), { answers: answers })
+
+    // const ref = await addDoc(collection(db, "__dev", "datastore", "answers"), JSON.stringify(answers))
+    // console.log('answers stored with ref: ', ref)
+  }
   
   return (
     <>
       {questions.map((q, i) => 
-        <Question key={i} text={q.text}/>
+        <Question key={i} text={q.text} identifier={q._id} onChange={(answer) => { onAnswerChange(q._id, answer) }}/>
       )}
-      <button className="bg-indigo-500 rounded-md p-2 mt-4">Submit!</button>
+      <SubmitButton onClick={processSubmission}/>
+      <div>
+        Current answers: 
+        {answers.length > 0 && (
+          <div className="text-sm">
+            {answers.map((a, i) => 
+              <span key={i}><b>{a.questionId}: </b> <span>{a.answerText}</span></span>
+            )}
+          </div>
+        )}
+        {answers.length === 0 && <span>No answers yet</span>}
+      </div>
     </>
   )
 }
@@ -71,9 +125,14 @@ function FormController() {
  * Renders an individual question, comprised of the question text
  * and an input for the answer.
  */
-function Question(props: { text: string }) {
+function Question(props: { text: string, identifier: string, onChange: () => void }) {
 
   const [answer, setAnswer] = useState('')
+
+  function handleChange(newValue) {
+    setAnswer(newValue)
+    props.onChange(newValue)
+  }
 
   return (
     <div className="">
@@ -82,9 +141,20 @@ function Question(props: { text: string }) {
         type="text" 
         className="block text-black" 
         value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
+        onChange={(e) => handleChange(e.target.value)}
       />
     </div>
+  )
+}
+
+function SubmitButton (props: { onClick: () => void }) {
+  return (
+    <button 
+      className="bg-indigo-500 rounded-md p-2 mt-4"
+      onClick={props.onClick}
+    >
+      Submit!
+    </button>
   )
 }
 
